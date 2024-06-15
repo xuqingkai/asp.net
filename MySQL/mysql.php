5bb4fb29bfa92b8ea5c130c6fc9615291240cc4b
徐清凯 <xuqingkai@hotmail.com>
2024-06-15 17:23:40 +0800

    Update mysql.php

--- MySQL/mysql.php
+++ MySQL/mysql.php
@@ -3,7 +3,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>MySQL</title>
+    <title>MYSQL</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 <body>
@@ -13,10 +13,22 @@
 	try
 	{
 		if (mySqlConnection.State != System.Data.ConnectionState.Open) { mySqlConnection.Open(); }
-		MySql.Data.MySqlClient.MySqlCommand mySqlCommand = new MySql.Data.MySqlClient.MySqlCommand(databaseSql, mySqlConnection);
-		mySqlCommand.ExecuteNonQuery();
-		ExecuteNonQuery("UPDATE [xqk_user] SET [nick_name]=1 WHERE [id]=1");
-		Response.Write("SUCCESS");
+        
+        string ip = System.Web.HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+        double timestamp = System.Math.Floor((System.DateTime.Now - System.Convert.ToDateTime("1970-01-01 00:00:00")).TotalSeconds);
+
+        string sql = "SELECT * FROM [xqk_ip_log] WHERE [ip]='" + ip + "' AND [create_timestamp]>" + (timestamp - 60*20)  + " ORDER BY ID DESC;";
+        MySql.Data.MySqlClient.MySqlCommand sqlCommand = new MySql.Data.MySqlClient.MySqlCommand(sql, sqlConnection);
+        System.Data.DataTable dataTable = new System.Data.DataTable();
+        new MySql.Data.MySqlClient.MySqlDataAdapter(sqlCommand).Fill(dataTable);
+        if (dataTable.Rows.Count == 0) {
+            sql = "INSERT INTO [xqk_ip_log] ([ip], [create_date], [create_datetime], [create_timestamp]) VALUES ('" + ip + "', '" + System.DateTime.Now.ToString("yyyy-MM-dd") + "', '" + System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "', " + timestamp + ");";
+            sqlCommand = new MySql.Data.MySqlClient.MySqlCommand(sql, sqlConnection);
+            int rows = sqlCommand.ExecuteNonQuery();
+        }else{
+            System.Data.DataRow dr = dataTable.Rows[0];
+            ip = dr["ip"].ToString();
+        }
 	}
 	catch(System.Exception exception)
 	{
